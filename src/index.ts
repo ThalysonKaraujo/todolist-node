a9d6113dc94190d09c87513d9e0a5fb20ddabf9f
Thalyson Kauan <thalyson.kauan7@gmail.com>
2025-11-01 21:10:22 -0300

    feat: add cookie-parser dependencie

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import 'dotenv/config';
 import { todosRoutes } from './routes/todos.routes.js';
 import { authRoutes } from './routes/auth.routes.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/todos', todosRoutes);
 app.use('/auth', authRoutes);
 app.use(errorHandler);
